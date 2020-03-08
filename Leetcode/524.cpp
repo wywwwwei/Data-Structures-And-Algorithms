e546dd2e2ad2938afee7683e452672931daac89e
wywwwwei <wyw1191448318@hotmail.com>
2020-03-09 00:36:58 +0800

    longest word in dictionary through deleting

--- Leetcode/524.cpp
+++ Leetcode/524.cpp
@@ -1,5 +1,5 @@
 /**
- * 524. 通过删除字母匹配到字典里最长单词 双指针逐个比较后，输出最长、字典序最小的字符串
+ * 524. 通过删除字母匹配到字典里最长单词 双指针逐个比较是否匹配，同时选出最长、字典序最小的字符串
  * link：https://leetcode-cn.com/problems/longest-word-in-dictionary-through-deleting/
  */
 
@@ -8,10 +8,9 @@ class Solution
 public:
     string findLongestWord(string s, vector<string> &d)
     {
-        vector<int> res_index;
         int s_len = s.length();
         int d_len = d.size();
-        //Select the matching string index
+        int res_index = -1;
         for (int i = 0; i < d_len; i++)
         {
             int index1 = 0, index2 = 0;
@@ -24,26 +23,11 @@ public:
                     index1++;
             }
             if (index2 == dic_len)
-                res_index.push_back(i);
-        }
-        if (res_index.empty())
-        {
-            return "";
-        }
-        //Pick the most suitable value
-        int res_len = res_index.size();
-        int max = res_index[0];
-        for (int i = 1; i < res_len; i++)
-        {
-            if (d[res_index[i]].length() > d[max].length())
             {
-                max = res_index[i];
-            }
-            else if (d[res_index[i]].length() == d[max].length() && d[res_index[i]] < d[max])
-            {
-                max = res_index[i];
+                if (res_index == -1 || d[res_index].length() < d[i].length() || (d[res_index].length() == d[i].length() && d[res_index] > d[i]))
+                    res_index = i;
             }
         }
-        return d[max];
+        return res_index == -1 ? "" : d[res_index];
     }
 };
