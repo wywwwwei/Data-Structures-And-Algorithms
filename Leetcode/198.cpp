483174dcb6f80c5244a9cacc09651aa4c2e8da1a
wywwwwei <wyw1191448318@hotmail.com>
2020-03-24 00:31:52 +0800

    house robber

---
+++ Leetcode/198.cpp
@@ -0,0 +1,23 @@
+/**
+ * 198. 打家劫舍 动态规划->遍历 同Interview1716
+ * link：https://leetcode-cn.com/problems/the-masseuse-lcci/
+ */
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        int pre = 0;
+        int cur = 0;
+
+        for (int &num : nums)
+        {
+            int temp = cur;
+            cur = max(cur, pre + num);
+            pre = temp;
+        }
+
+        return cur;
+    }
+};
