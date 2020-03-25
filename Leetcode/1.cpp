544908cbe80dfa317c335b04b66473a5cd65b689
wywwwwei <wyw1191448318@hotmail.com>
2020-03-26 00:56:16 +0800

    two sum

---
+++ Leetcode/1.cpp
@@ -0,0 +1,18 @@
+/**
+ * 1. 两数之和
+ * link：https://leetcode-cn.com/problems/two-sum/
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> record;
+
+        for(int i = 0;i < nums.size();i++){
+            if(record.find(target - nums[i]) != record.end())return {record[target - nums[i]],i};
+            record[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
