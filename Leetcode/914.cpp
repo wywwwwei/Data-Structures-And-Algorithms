5476fb6ae6aa32a25ad7879bc78a876f35145de7
wywwwwei <wyw1191448318@hotmail.com>
2020-03-27 01:39:11 +0800

    x of a kind in a deck of cards

--- Leetcode/914.cpp
+++ Leetcode/914.cpp
@@ -3,16 +3,21 @@
  * link：https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards/
  */
 
-class Solution {
+class Solution
+{
 public:
-    bool hasGroupsSizeX(vector<int>& deck) {
-        unordered_map<int,int> group;
-        for(const int &num : deck)group[num]++;
+    bool hasGroupsSizeX(vector<int> &deck)
+    {
+        unordered_map<int, int> group;
+        for (const int &num : deck)
+            group[num]++;
 
         int result = 0;
-        for(const auto &num : group){
-            result = __gcd(result,num.second);
-            if(result < 2)return false;
+        for (const auto &num : group)
+        {
+            result = __gcd(result, num.second);
+            if (result < 2)
+                return false;
         }
 
         return true;
