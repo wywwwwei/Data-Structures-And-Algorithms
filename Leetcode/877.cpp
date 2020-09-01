8119a058e8ce16ab0b09f9f44aa77569aa4087c6
wywwwwei <wyw1191448318@hotmail.com>
2020-09-01 14:36:50 +0800

    stone game

---
+++ Leetcode/877.cpp
@@ -0,0 +1,24 @@
+/**
+ * 877. 石子游戏
+ * link：https://leetcode-cn.com/problems/stone-game/
+ */
+
+/**
+ * Because piles.length is even,
+ * say the first, third, fifth, seventh, etc. piles are white, 
+ * and the second, fourth, sixth, eighth, etc. piles are black.
+ * 
+ * Alex(the first hand) can always take either all white piles or all black piles, 
+ * and one of the colors must have a sum number of stones larger than the other color.
+ * 
+ * Hence, Alex(the first hand) always wins the game.
+ */
+
+class Solution
+{
+public:
+    bool stoneGame(vector<int> &piles)
+    {
+        return true;
+    }
+};
