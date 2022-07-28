fd2d6138d50f539251304d9462f48993de95b7c7
Wu Yongwei <wyw1191448318@hotmail.com>
2022-07-29 01:45:49 +0800

    valid square

--- Leetcode/593.js
+++ Leetcode/593.js
@@ -11,17 +11,17 @@
  * @return {boolean}
  */
 var validSquare = function (p1, p2, p3, p4) {
-    const edgeLenghSquare = function (p1, p2) {
+    const edgeLengthSquare = function (p1, p2) {
         return Math.abs(p1[0] - p2[0]) ** 2 + Math.abs(p1[1] - p2[1]) ** 2;
     }
 
     const edgeLengthArray = Array(6).fill(0);
-    edgeLengthArray[0] = edgeLenghSquare(p1, p2);
-    edgeLengthArray[1] = edgeLenghSquare(p1, p3);
-    edgeLengthArray[2] = edgeLenghSquare(p1, p4);
-    edgeLengthArray[3] = edgeLenghSquare(p2, p3);
-    edgeLengthArray[4] = edgeLenghSquare(p2, p4);
-    edgeLengthArray[5] = edgeLenghSquare(p3, p4);
+    edgeLengthArray[0] = edgeLengthSquare(p1, p2);
+    edgeLengthArray[1] = edgeLengthSquare(p1, p3);
+    edgeLengthArray[2] = edgeLengthSquare(p1, p4);
+    edgeLengthArray[3] = edgeLengthSquare(p2, p3);
+    edgeLengthArray[4] = edgeLengthSquare(p2, p4);
+    edgeLengthArray[5] = edgeLengthSquare(p3, p4);
 
     edgeLengthArray.sort((a, b) => (a - b));
     return edgeLengthArray[0] === edgeLengthArray[3] && edgeLengthArray[4] === edgeLengthArray[5] && edgeLengthArray[3] < edgeLengthArray[4];
