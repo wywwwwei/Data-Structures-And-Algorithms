c9fbcd89ed4f1fb0e590b2a2d559a87aacb426f1
Wu Yongwei <wyw1191448318@hotmail.com>
2022-08-28 23:54:36 +0800

    preimage size of factorial zeroes function

---
+++ Leetcode/793.js
@@ -0,0 +1,35 @@
+/**
+ * 793. 阶乘函数后 K 个零
+ * link：https://leetcode.cn/problems/preimage-size-of-factorial-zeroes-function/
+ */
+
+/**
+ * @param {number} k
+ * @return {number}
+ */
+var preimageSizeFZF = function (k) {
+    return help(k + 1) - help(k);
+};
+
+const help = (k) => {
+    let r = 5 * k;
+    let l = 0;
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2);
+        if (zeta(mid) < k) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    return r + 1;
+}
+
+const zeta = (x) => {
+    let res = 0;
+    while (x != 0) {
+        res += Math.floor(x / 5);
+        x = Math.floor(x / 5);
+    }
+    return res;
+};
